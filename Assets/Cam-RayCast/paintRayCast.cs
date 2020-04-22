10c0c0d5236064d73a3d163ac2cb0f793939ca4a
MrFlou <Simonflou@gmail.com>
2020-04-22 15:46:11 +0200

    HEATMAP

--- Assets/Cam-RayCast/paintRayCast.cs
+++ Assets/Cam-RayCast/paintRayCast.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 using System;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 
 
 public class paintRayCast : MonoBehaviour
@@ -10,10 +14,11 @@ public class paintRayCast : MonoBehaviour
     public Camera Camera;
     public int res;
     int ID = 0;
-    int picNr = 0;
+    int picNr = 1;
     RaycastHit hit;
-    int[,] imageArray = new int[512, 512];
-    int[,] takenArray;
+    float[,] imageArray = new float[512, 512];
+    float[,] takenArray;
+
 
 
     // Start is called before the first frame update
@@ -42,7 +47,7 @@ public class paintRayCast : MonoBehaviour
 
     void TakeImage()
     {
-        takenArray = new int[512, 512];
+        
         float camFOV = Camera.fieldOfView / 2;
         float difDeg = ((camFOV * 2) / res);
         Vector3 directionRay = Quaternion.AngleAxis(camFOV, new Vector3(0, 1, 0)) * Vector3.forward;
@@ -51,6 +56,8 @@ public class paintRayCast : MonoBehaviour
         Renderer rend = hit.transform.GetComponent<Renderer>();
         Texture2D tex = rend.material.mainTexture as Texture2D;
 
+
+        takenArray = new float[512, 512];
         float nowDegY = 0f;        
         for (int y = 0; y < res; y++)
         {
@@ -69,25 +76,38 @@ public class paintRayCast : MonoBehaviour
             
             nowDegY = nowDegY + difDeg;            
         }
+        Texture2D textureOut = new Texture2D(512, 512);
         for (int y = 0; y < takenArray.GetLength(0); y++)
             {
                 for (int x = 0; x < takenArray.GetLength(1); x++)
                 {
-                    imageArray[x, y] = imageArray[x, y] + takenArray[x, y];
-                    if (imageArray[x,y] == 1)
-                    {
-                        tex.SetPixel(x, y, Color.red);
-                    } else if (imageArray[x, y] == 2)
-                    {
-                        tex.SetPixel(x, y, Color.blue);
-                    } else if (imageArray[x, y] >= 3)
-                    {
-                        tex.SetPixel(x, y, Color.green);
-                    }
+                    imageArray[x, y] = imageArray[x, y] + takenArray[x, y];                    
+                    Color col = new Color(imageArray[x, y] / picNr, imageArray[x, y] / picNr, imageArray[x, y] / picNr, 1f);                    
+                    textureOut.SetPixel(x, y, col);
+                /* Standard Red,Blue,Green
+                 if (takenArray[x,y] == 1)
+                {
+                    tex.SetPixel(x, y, Color.red);
+                } else if (imageArray[x, y] == 2)
+                {
+                    tex.SetPixel(x, y, Color.blue);
+                } else if (imageArray[x, y] >= 3)
+                {
+                    tex.SetPixel(x, y, Color.green);
+                }*/
                 }
             }
+
+        SaveTextureAsPNG(textureOut, Application.dataPath + "/grayScale.png");
         tex.Apply();
         picNr = picNr + 1;
 
     }
+    public static void SaveTextureAsPNG(Texture2D _texture, string _fullPath)
+    {
+        byte[] _bytes = _texture.EncodeToPNG();
+        System.IO.File.WriteAllBytes(_fullPath, _bytes);
+        Debug.Log(_bytes.Length / 512 + "Kb was saved as: " + _fullPath);
+    }
+
 }
